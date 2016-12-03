3419527ed1f7d0afff81d3d91b3fa843f73719da
Syafiqq <syafiq.rezpector@gmail.com>
2016-12-03 22:13:52 +0700

    Fix login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -13,16 +13,20 @@
 <head>
 
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Login</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="../../../apple-touch-icon.png">
 
     <!-- CSS -->
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
-    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/auth/login/bootstrap/css/bootstrap.min.css')?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/auth/login/font-awesome/css/font-awesome.min.css')?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/auth/login/css/form-elements.css')?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/auth/login/css/style.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/bower_components/bootstrap/dist/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/bower_components/animate.css/animate.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/auth/login/font-awesome/css/font-awesome.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/auth/login/css/form-elements.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/auth/login/css/style.css') ?>">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -39,132 +43,48 @@
 
     <div class="inner-bg">
         <div class="container">
-
             <div class="row">
-                <div class="col-sm-8 col-sm-offset-2 text">
-                    <h1><strong>Bootstrap</strong> Login &amp; Register Forms</h1>
-                    <div class="description">
-                        <p>
-                            This is a free responsive <strong>"login and register forms"</strong> template made with Bootstrap.
-                            Download it on <a href="http://azmind.com" target="_blank"><strong>AZMIND</strong></a>,
-                            customize and use it as you like!
-                        </p>
-                    </div>
+                <div id="notif" class="col-md-12">
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-5">
-
+                <div class="col-md-4 offset-md-4">
                     <div class="form-box">
                         <div class="form-top">
                             <div class="form-top-left">
                                 <h3>Login to our site</h3>
-                                <p>Enter username and password to log on:</p>
+                                <p>Please fill the form :</p>
                             </div>
                             <div class="form-top-right">
                                 <i class="fa fa-lock"></i>
                             </div>
                         </div>
                         <div class="form-bottom">
-                            <form role="form" action="" method="post" class="login-form">
+                            <form role="form" action="<?php echo site_url('auth/do_login') ?>" method="post" class="login-form" id="login_form">
                                 <div class="form-group">
                                     <label class="sr-only" for="form-username">Username</label>
-                                    <input type="text" name="form-username" placeholder="Username..." class="form-username form-control" id="form-username">
+                                    <input type="text" name="email" placeholder="Email..." class="form-username form-control" id="form-username" value="syafiq.rezpector@gmail.com">
                                 </div>
                                 <div class="form-group">
                                     <label class="sr-only" for="form-password">Password</label>
-                                    <input type="password" name="form-password" placeholder="Password..." class="form-password form-control" id="form-password">
+                                    <input type="password" name="password" placeholder="Password..." class="form-password form-control" id="form-password" value="Muhammad_Syafiq">
                                 </div>
-                                <button type="submit" class="btn">Sign in!</button>
+                                <button type="submit" class="btn">Log in!</button>
                             </form>
                         </div>
                     </div>
-
-                    <div class="social-login">
-                        <h3>...or login with:</h3>
-                        <div class="social-login-buttons">
-                            <a class="btn btn-link-2" href="#">
-                                <i class="fa fa-facebook"></i> Facebook
-                            </a>
-                            <a class="btn btn-link-2" href="#">
-                                <i class="fa fa-twitter"></i> Twitter
-                            </a>
-                            <a class="btn btn-link-2" href="#">
-                                <i class="fa fa-google-plus"></i> Google Plus
-                            </a>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="col-sm-1 middle-border"></div>
-                <div class="col-sm-1"></div>
-
-                <div class="col-sm-5">
-
-                    <div class="form-box">
-                        <div class="form-top">
-                            <div class="form-top-left">
-                                <h3>Sign up now</h3>
-                                <p>Fill in the form below to get instant access:</p>
-                            </div>
-                            <div class="form-top-right">
-                                <i class="fa fa-pencil"></i>
-                            </div>
-                        </div>
-                        <div class="form-bottom">
-                            <form role="form" action="" method="post" class="registration-form">
-                                <div class="form-group">
-                                    <label class="sr-only" for="form-first-name">First name</label>
-                                    <input type="text" name="form-first-name" placeholder="First name..." class="form-first-name form-control" id="form-first-name">
-                                </div>
-                                <div class="form-group">
-                                    <label class="sr-only" for="form-last-name">Last name</label>
-                                    <input type="text" name="form-last-name" placeholder="Last name..." class="form-last-name form-control" id="form-last-name">
-                                </div>
-                                <div class="form-group">
-                                    <label class="sr-only" for="form-email">Email</label>
-                                    <input type="text" name="form-email" placeholder="Email..." class="form-email form-control" id="form-email">
-                                </div>
-                                <div class="form-group">
-                                    <label class="sr-only" for="form-about-yourself">About yourself</label>
-                                    <textarea name="form-about-yourself" placeholder="About yourself..."
-                                              class="form-about-yourself form-control" id="form-about-yourself"></textarea>
-                                </div>
-                                <button type="submit" class="btn">Sign me up!</button>
-                            </form>
-                        </div>
-                    </div>
-
                 </div>
             </div>
-
         </div>
     </div>
-
 </div>
 
-<!-- Footer -->
-<footer>
-    <div class="container">
-        <div class="row">
-
-            <div class="col-sm-8 col-sm-offset-2">
-                <div class="footer-border"></div>
-                <p>Made by Anli Zaimi at <a href="http://azmind.com" target="_blank"><strong>AZMIND</strong></a>
-                   having a lot of fun. <i class="fa fa-smile-o"></i></p>
-            </div>
-
-        </div>
-    </div>
-</footer>
-
 <!-- Javascript -->
-<script src="<?php echo base_url('assets/frontend/auth/login/js/jquery-1.11.1.min.js')?>"></script>
-<script src="<?php echo base_url('assets/frontend/auth/login/bootstrap/js/bootstrap.min.js')?>"></script>
-<script src="<?php echo base_url('assets/frontend/auth/login/js/jquery.backstretch.min.js')?>"></script>
-<script src="<?php echo base_url('assets/frontend/auth/login/js/scripts.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/jquery/dist/jquery.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/jquery-serialize-object/dist/jquery.serialize-object.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/tether/dist/js/tether.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/remarkable-bootstrap-notify/dist/bootstrap-notify.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/auth/login/js/jquery.backstretch.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/frontend/auth/login/js/scripts.js') ?>"></script>
 
 <!--[if lt IE 10]>
 <script src="<?php echo base_url('assets/frontend/auth/login/js/placeholder.js')?>"></script>
