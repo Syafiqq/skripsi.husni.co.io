ad897857bb030bd52771b5359d63611e4b5a372c
Syafiqq <syafiq.rezpector@gmail.com>
2016-12-05 06:55:29 +0700

    remove unnecessary script

--- application/views/story/detail/user.php
+++ application/views/story/detail/user.php
@@ -21,7 +21,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Dashboard</title>
+    <title>Story Detail</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
@@ -595,103 +595,6 @@ desired effect
 <script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/jquery-slimscroll/jquery.slimscroll.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/frontend/bower_components/fastclick/lib/fastclick.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/frontend/story/detail/js/user.js') ?>"></script>
-<script type="text/javascript">
-    (function ($)
-    {
-
-        $(function ()
-        {
-            $('input#generate-rating').rating({
-                displayOnly: false,
-                size: 'xs',
-                stars: 10,
-                showCaption: false,
-                showClear: false,
-                max: 10,
-                animate: true
-            }).on("rating.change", function (event, value, caption)
-            {
-                var form = $('input#generate-rating');
-                $.ajax({
-                    type: form.attr('method'),
-                    url: form.attr('action'),
-                    data: {value: value},
-                    dataType: 'json',
-                    contentType: 'application/x-www-form-urlencoded; charset=UTF-8; X-Requested-With: XMLHttpRequest'
-                })
-                    .done(function (data)
-                    {
-                        if (data.hasOwnProperty('data'))
-                        {
-                            if (data['data'].hasOwnProperty('notify'))
-                            {
-                                var notify = data['data']['notify'];
-                                for (var i = -1; ++i < notify.length;)
-                                {
-                                    $.notify({message: notify[i][0]}, {type: notify[i][1]});
-                                }
-                            }
-                        }
-                    })
-                    .fail(function ()
-                    {
-                        $.notify({
-                            message: 'Error'
-                        }, {
-                            type: 'danger'
-                        });
-                    });
-            });
-
-            $('form#mark_story').on("submit", function (event)
-            {
-                event.preventDefault();
-                var form = $(this);
-                $.ajax({
-                    type: form.attr('method'),
-                    url: form.attr('action'),
-                    dataType: 'json',
-                    contentType: 'application/x-www-form-urlencoded; charset=UTF-8; X-Requested-With: XMLHttpRequest'
-                })
-                    .done(function (data)
-                    {
-                        if (data.hasOwnProperty('data'))
-                        {
-                            if (data['data'].hasOwnProperty('notify'))
-                            {
-                                var notify = data['data']['notify'];
-                                for (var i = -1; ++i < notify.length;)
-                                {
-                                    $.notify({message: notify[i][0]}, {type: notify[i][1]});
-                                }
-                            }
-                            if (data.hasOwnProperty('code'))
-                            {
-                                if (data['code'] == 200)
-                                {
-                                    setTimeout(function ()
-                                    {
-                                        location.reload();
-                                    }, 2000);
-                                }
-                            }
-                        }
-                    })
-                    .fail(function ()
-                    {
-                        $.notify({
-                            message: 'Error'
-                        }, {
-                            type: 'danger'
-                        });
-                    });
-            });
-        });
-        /*
-         * Run right away
-         * */
-    })(jQuery);
-</script>
 </body>
 </html>
 
