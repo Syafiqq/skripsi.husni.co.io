528417ba4fb8a8caf4e225558d181960a1b9307f
Syafiqq <syafiq.rezpector@gmail.com>
2016-12-05 06:55:38 +0700

    migrate script

--- assets/frontend/story/detail/js/user.js
+++ assets/frontend/story/detail/js/user.js
@@ -56,6 +56,92 @@
                     });
                 })
         });
+
+        $('input#generate-rating').rating({
+            displayOnly: false,
+            size: 'xs',
+            stars: 10,
+            showCaption: false,
+            showClear: false,
+            max: 10,
+            animate: true
+        }).on("rating.change", function (event, value, caption)
+        {
+            var form = $('input#generate-rating');
+            $.ajax({
+                type: form.attr('method'),
+                url: form.attr('action'),
+                data: {value: value},
+                dataType: 'json',
+                contentType: 'application/x-www-form-urlencoded; charset=UTF-8; X-Requested-With: XMLHttpRequest'
+            })
+                .done(function (data)
+                {
+                    if (data.hasOwnProperty('data'))
+                    {
+                        if (data['data'].hasOwnProperty('notify'))
+                        {
+                            var notify = data['data']['notify'];
+                            for (var i = -1; ++i < notify.length;)
+                            {
+                                $.notify({message: notify[i][0]}, {type: notify[i][1]});
+                            }
+                        }
+                    }
+                })
+                .fail(function ()
+                {
+                    $.notify({
+                        message: 'Error'
+                    }, {
+                        type: 'danger'
+                    });
+                });
+        });
+
+        $('form#mark_story').on("submit", function (event)
+        {
+            event.preventDefault();
+            var form = $(this);
+            $.ajax({
+                type: form.attr('method'),
+                url: form.attr('action'),
+                dataType: 'json',
+                contentType: 'application/x-www-form-urlencoded; charset=UTF-8; X-Requested-With: XMLHttpRequest'
+            })
+                .done(function (data)
+                {
+                    if (data.hasOwnProperty('data'))
+                    {
+                        if (data['data'].hasOwnProperty('notify'))
+                        {
+                            var notify = data['data']['notify'];
+                            for (var i = -1; ++i < notify.length;)
+                            {
+                                $.notify({message: notify[i][0]}, {type: notify[i][1]});
+                            }
+                        }
+                        if (data.hasOwnProperty('code'))
+                        {
+                            if (data['code'] == 200)
+                            {
+                                setTimeout(function ()
+                                {
+                                    location.reload();
+                                }, 2000);
+                            }
+                        }
+                    }
+                })
+                .fail(function ()
+                {
+                    $.notify({
+                        message: 'Error'
+                    }, {
+                        type: 'danger'
+                    });
+                });
+        });
     });
 
     /*
