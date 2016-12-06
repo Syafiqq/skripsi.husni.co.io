5f55ab048757a982780f25ff545044fd0b4c69e5
Syafiqq <syafiq.rezpector@gmail.com>
2016-12-06 15:13:46 +0700

    change index_page

--- application/config/config.php
+++ application/config/config.php
@@ -35,8 +35,8 @@ $config['base_url'] = 'http://skripsi.esy.es/';
 | variable so that it is blank.
 |
 */
-//$config['index_page'] = 'index.php';
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
+//$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
